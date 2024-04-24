d7c385108dfde983a7528f7f74d52461618be237
Mike <149283058+legendaryjones@users.noreply.github.com>
2024-04-24 15:58:40 -0500

    Add files via upload

---
+++ Lesson3s.py
@@ -0,0 +1,27 @@
+
+grades = [85, 90, 78, 88, 76, 95, 89, 80, 72, 93]
+grades.sort (reverse= True)
+print (grades)
+
+average= int (sum(grades) /len(grades))
+
+print(average)
+
+temperatures = [72, 75, 78, 79, 80, 81, 82, 83, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106]
+
+print(temperatures[7:13])
+
+print (temperatures[23:30])
+
+temperatures2= [72, 75, 78, 79, 80, 81, 82, 83, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106]
+temperatures2.sort (reverse=True)
+print (temperatures2)
+print(temperatures2[5:9])
+
+students = ["John", "Doe", "Jane", "Smith"]
+grades = [85, 90, 78, 88]
+activities = ["Football", "Music", "Art", "Dance"]
+print(students[2], grades[2],activities[2])
+
+students_approved= [students[0], students [1], students [3]]
+print(students_approved)
